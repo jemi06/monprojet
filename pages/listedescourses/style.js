00debbdf6722116052c3fc3d42c8ce85fb14833c
jeremie callay <jeremie.callay@gmail.com>
2022-01-28 12:00:18 +0100

    28012022

--- pages/listedescourses/style.js
+++ pages/listedescourses/style.js
@@ -1,47 +1,48 @@
-//ajouter ingredient et GRAMMES
-function ajouter(){
-  document.getElementById("divers").innerHTML =  `<label id="ing" for="ingredient">INGREDIENTS : </label><input id="ingredient" type="text">
-  <label id="quant" for="quantite">GRAMMES: </label><input id="quantite" type="number"><label id="supp" for="supping"> </label><button id="supping" onclick="supprimer()"> delete </button><br><br>
-  <label for="Valider">VALIDER LA RECETTE : </label><button id="Valide" onclick="divrecette()"> VALIDER</button>`;
+// ajouter ingredient et GRAMMES
 
+function ajouter() {
+    document.getElementById("divers").innerHTML = `<label id="ing" for="ing">INGREDIENTS : </label><input id="ingredient" type="text">
+  <label id="quant" for="quantite">GRAMMES: </label><input id="quantite" type="number"><button id="ajouter" onclick="ajouter()">+</button><label id="supp" for="supping"> </label><button id="supping" onclick="supprimer()"> delete </button>`;
+    document.getElementById("divers" + 1).innerHTML = (i <= 10) ? `<label id="ing" for="ing">INGREDIENTS : </label><input id="ingredient" type="text">
+  <label id="quant" for="quantite">GRAMMES: </label><input id="quantite" type="number"><button id="ajouter" onclick="ajouter()">+</button><label id="supp" for="supping"> </label><button id="supping" onclick="supprimer()"> delete </button>`;
 }
 
-function supprimer(){
-  document.getElementById ("ingredient").remove();
-  document.getElementById ("quantite").remove();
-  document.getElementById ("ing").remove();
-  document.getElementById ("quant").remove();
-  document.getElementById ("supping").remove();
-  document.getElementById ("supp").remove();
-  document.getElementById ("valide").remove();
-  document.getElementById ("valider").remove();
+
+
+function supprimer() {
+    document.getElementById("ingredient").remove();
+    document.getElementById("quantite").remove();
+    document.getElementById("ing").remove();
+    document.getElementById("quant").remove();
+    document.getElementById("supping").remove();
+    document.getElementById("supp").remove();
 
 }
 
 
 class recette {
-  constructor(nomrecette, nbpersonne, temperature, tempcuit )    {
-      this.nomrecette = nomrecette;
-      this.nbpers = nbpersonne;
-      this.temp = temperature;
-      this.tempcuit = tempcuit;
-  }
- 
-infosrecette() {
-      document.getElementById('toto').innerHTML +=
-          'la recette  ' + $(this.nomrecette) + ' nombre personnes:  ' + $(this.nbpers) + ` temperature: ` + $(this.temp) + ` minute: ` + $(this.tempcuit) + '<br>'
-  }
-}
+    constructor(nomrecette, nbpers, temp, tempcuit, ingredient, quantite) {
+        this.nomrecette = nomrecette;
+        this.nbpers = nbpers;
+        this.temp = temp;
+        this.tempcuit = tempcuit;
+        this.ingredient = ingredient;
+        this.quantite = quantite;
+    }
 
-function divrecette (){
-  let nomrecette = document.getElementById("nomrecette").value;
-  let nbpers = parseFloat(document.getElementById("nbpers")).value;
-  let temp = parseFloat(document.getElementById("temperature")).value;
-  let tempcuit = parseFloat(document.getElementById("tempcuit")).value;
-  let ingredient = document.getElementById("ingredient").value;
-  let quantite = parseFloat(document.getElementById("quantite")).value;
-  
-  let newrecette= newRecette (nomrecette, nbpers, temp, tempcuit, ingredient, quantite );
-  newrecette.infosrecette();
+    infosrecette() {
+        document.getElementById("toto").innerHTML = `la recette :  ${this.nomrecette} <br> nombre personnes :  ${this.nbpers} <br> temperature: ${this.temp} <br> minute:   ${this.tempcuit} <br> ingredient: ${this.ingredient}  gramme:  ${this.quantite} <br> `
+    };
+};
 
+
+function divrecette() {
+    let nomrecette = document.getElementById("nomrecette").value;
+    let nbpers = document.getElementById("nbpers").value;
+    let temp = document.getElementById("temp").value;
+    let tempcuit = document.getElementById("tempcuit").value;
+    let ingredient = document.getElementById("ingredient").value;
+    let quantite = document.getElementById("quantite").value;
+    let newrec = new recette(nomrecette, nbpers, temp, tempcuit, ingredient, quantite);
+    newrec.infosrecette();
 }
