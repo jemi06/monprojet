8f9819b9578d255f50c6b43927f7a56932792ba2
jeremie callay <jeremie.callay@gmail.com>
2022-01-31 16:22:41 +0100

    31012022

--- pages/listedescourses/style.js
+++ pages/listedescourses/style.js
@@ -1,22 +1,29 @@
-// ajouter ingredient et GRAMMES
+// ajouter ingredient et GRAMMES+
+let cpt = 1;
 
 function ajouter() {
-    document.getElementById("divers").innerHTML = `<label id="ing" for="ing">INGREDIENTS : </label><input id="ingredient" type="text">
-  <label id="quant" for="quantite">GRAMMES: </label><input id="quantite" type="number"><button id="ajouter" onclick="ajouter()">+</button><label id="supp" for="supping"> </label><button id="supping" onclick="supprimer()"> delete </button>`;
-    document.getElementById("divers" + 1).innerHTML = (i <= 10) ? `<label id="ing" for="ing">INGREDIENTS : </label><input id="ingredient" type="text">
-  <label id="quant" for="quantite">GRAMMES: </label><input id="quantite" type="number"><button id="ajouter" onclick="ajouter()">+</button><label id="supp" for="supping"> </label><button id="supping" onclick="supprimer()"> delete </button>`;
-}
 
 
+    let ingredient = `ingredient${cpt}`;
+    let quantite = `quantite${cpt}`;
+    console.log(ingredient);
+    console.log(quantite);
 
-function supprimer() {
-    document.getElementById("ingredient").remove();
-    document.getElementById("quantite").remove();
-    document.getElementById("ing").remove();
-    document.getElementById("quant").remove();
-    document.getElementById("supping").remove();
-    document.getElementById("supp").remove();
 
+    document.getElementById("divers").innerHTML += `<br><label for=ingredient>INGREDIENTS : </label><input id=ingredient type="text">
+    <label for=quantite>GRAMMES: </label><input id=quantite type="text"><button id="ajouter" onclick="ajouter()">+</button><label id="supp" for="supping"> </label><button id="supping" onclick="supprimer()"> delete </button><br>`;
+    cpt++;
+
+    // function newFunction() {
+    //     let cpt = 0;
+    //     let ingredient = "ingredient" + cpt;
+    //     let quantite = "quant" + cpt;
+
+    //     for (i = 0; i < cpt; i++) {
+    //         ingredient = "ingredient" + cpt;
+    //         quantite = "quantite" + cpt;
+    //     }
+    // }
 }
 
 
@@ -29,11 +36,28 @@ class recette {
         this.ingredient = ingredient;
         this.quantite = quantite;
     }
-
     infosrecette() {
-        document.getElementById("toto").innerHTML = `la recette :  ${this.nomrecette} <br> nombre personnes :  ${this.nbpers} <br> temperature: ${this.temp} <br> minute:   ${this.tempcuit} <br> ingredient: ${this.ingredient}  gramme:  ${this.quantite} <br> `
-    };
+        document.getElementById("toto").innerHTML = `la recette :  ${this.nomrecette} <br> nombre personnes :  ${this.nbpers} <br> temperature: ${this.temp} <br> minute:   ${this.tempcuit} <br>  `
+        for (i = 1; i <= cpt; i++) {
+            document.getElementById("toto").innerHTML += `<br> ingredient: ${this.ingredient}  gramme:  ${this.quantite} `
+        }
+
+    }
+
 };
+
+// class info extends recette {
+//     constructor(ingredient, quantite) {
+//         super(nomrecette, nbpers, temp, tempcuit, ingredient, quantite); //Appelle le constructeur 
+//         // parent
+//         this.ingredient = ingredient;
+//         this.quantite = quantite;
+//     }
+//     ingrequant() {
+//         document.getElementById("toto2").innerHTML = `ingredient: ${this.ingredient}  gramme:  ${this.quantite}  `
+//         console.log(this.ingredient)
+//     };
+// };
 
 
 function divrecette() {
@@ -41,8 +65,38 @@ function divrecette() {
     let nbpers = document.getElementById("nbpers").value;
     let temp = document.getElementById("temp").value;
     let tempcuit = document.getElementById("tempcuit").value;
-    let ingredient = document.getElementById("ingredient").value;
-    let quantite = document.getElementById("quantite").value;
+
+
+    for (i = 1; i <= cpt; i++) {
+        ingredient = `ingredient${i}`;
+        quantite = `quantite${i}`;
+        var ingredient = document.getElementById('ingredient').value;
+        var quantite = document.getElementById('quantite').value;
+        console.log(ingredient)
+        console.log(quantite);
+
+    }
+
     let newrec = new recette(nomrecette, nbpers, temp, tempcuit, ingredient, quantite);
     newrec.infosrecette();
 }
+
+function supprimer() {
+    document.getElementById("ingredient").remove();
+    document.getElementById("quantite").remove();
+    document.getElementById("supping").remove();
+    document.getElementById("supp").remove();
+    document.getElementById("ajouter").remove();
+
+
+
+}
+
+// C'est toujours le même problème, comême id ça va juste chercher le premier "id" de la page donc soit faut faire des id différents quand tu les crées, soit fonctionner avec des classes (css)me ils ont le m
+// pourquoi ajouter dans le class css
+// C'est une class en html
+
+// En faite un "id" tu es censé en avoir qu'un seul identique dans ta page, donc si tu en as plusieurs ça fonctionnera pas
+
+// alors que quand tu fais 
+// getElementsByClassname tu recuperes tout les elements avec la même classe (et pas juste un)
